bb126a2f43b847fc9df9db3795472d6c278a9bfa
eshill1 <71250941+eshill1@users.noreply.github.com>
2020-10-03 10:42:49 -0700

    Created AddingMachine class skeleton.

---
+++ CSE360Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,56 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	  private int total;
+	  
+	  
+	  /*
+	   * Default Constructor --> total = 0
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /* getTotal --> gets the private value
+	   * @return total --> 0
+	   */
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /* 
+	   * add --> increments total by parameter
+	   *  @param int value --> what the total is incremented by
+	   *  @return void
+	   */
+	  
+	  public void add (int value) {
+	  }
+	  
+	  
+	  /* 
+	   * subtract --> decrements total by parameter
+	   *  @param int value --> what the total is decremented by
+	   *  @return void
+	   */
+	  public void subtract (int value) {
+	  }
+	  
+	  
+	  /*
+	   * toString --> writes out the total of the object
+	   * @return String that has the total value
+	   */
+	 public String toString () {
+	    return "";
+	  }
+	  
+	  
+	 /*
+	  * clears total
+	  * @return void
+	  */
+	public void clear() {
+	  }
+	}
